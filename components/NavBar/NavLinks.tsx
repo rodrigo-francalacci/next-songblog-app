533a34f1c72be096156530b6e7b16896a45132b6
unknown <rodrigo.francalacci@gmail.com>
2022-06-14 18:55:22 +0100

    nav links fixed

--- components/NavBar/NavLinks.tsx
+++ components/NavBar/NavLinks.tsx
@@ -1,5 +1,6 @@
 import styles from './NavLinks.module.css';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 
 export default function NavLinks(props){
@@ -9,49 +10,63 @@ export default function NavLinks(props){
 
 
 return(
+
+    
     
     <ul className={`${styles.NavLinksList}`}>
     <motion.li
         initial={animateFrom}
         animate={animateTo}
         transition={{delay: 0.30}}
-        onClick={() => props.isMobile && props.closeMobileMenu()}> 
-        <a className={`size-49`} href="/#home">Home</a>
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
+            <Link href="/#home">
+            <a className={`size-49`}>Home</a>
+            </Link>
     </motion.li>
 <motion.li
         initial={animateFrom}
         animate={animateTo}
         transition={{delay: 0.30}}
         onClick={() => props.isMobile && props.closeMobileMenu()}> 
-        <a className={`size-49`} href="/#campaign">{props.campaignTitle}</a>
+            <Link href="/#campaign">
+            <a className={`size-49`}>{props.campaignTitle}</a>
+            </Link>
     </motion.li>
 <motion.li
         initial={animateFrom}
         animate={animateTo}
         transition={{delay: 0.45}}
         onClick={() => props.isMobile && props.closeMobileMenu()}> 
-        <a className={`size-49`} href="/#works">Works</a>
+            <Link href="/#works">
+            <a className={`size-49`}>Works</a>
+            </Link>
     </motion.li>
     <motion.li
         initial={animateFrom}
         animate={animateTo}
         transition={{delay: 0.65}}
         onClick={() => props.isMobile && props.closeMobileMenu()}> 
-        <a className={`size-49`} href="/#about">About</a>
+            <Link href="/#about">
+            <a className={`size-49`}>About</a>
+            </Link>
     </motion.li>
     <motion.li
         initial={animateFrom}
         animate={animateTo}
         transition={{delay: 0.65}}
-        onClick={() => props.isMobile && props.closeMobileMenu()}> 
-        <a className={`size-49`} href="/#blog">Blog</a>
+        onClick={() => props.isMobile && props.closeMobileMenu()}>
+            <Link href="/#blog">
+            <a className={`size-49`}>Blog</a>
+            </Link>
     </motion.li>
     <motion.li
         initial={animateFrom}
         animate={animateTo}
         transition={{delay: 0.90}}
         onClick={() => props.isMobile && props.closeMobileMenu()}> 
-        <a className={`size-49`} href="/#contact">Contacts</a>
+            <Link href="/#contact">
+            <a className={`size-49`}>Contacts</a>
+            </Link>
     </motion.li>
 </ul>
     
